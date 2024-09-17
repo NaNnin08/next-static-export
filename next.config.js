249f3ce4de7c6d02c3573ffcdef6f5bb07a2deff
nida <nida@code.id>
2024-09-17 14:18:27 +0700

    feat: change metadata #4

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true, // Optional jika menggunakan Next.js Images, GitHub Pages tidak mendukung Image Optimization.
   },
-  assetPrefix: "./", // Ini untuk memastikan resource diakses dengan path yang benar di GitHub Pages.
-  basePath: "/next-static-export", // Ganti dengan nama repositori GitHub Anda
+  assetPrefix: process.env.NEXT_PUBLIC_MODE === "dev" ? "" : "./", // Ini untuk memastikan resource diakses dengan path yang benar di GitHub Pages.
+  basePath: process.env.NEXT_PUBLIC_MODE === "dev" ? "" : "/next-static-export", // Ganti dengan nama repositori GitHub Anda
 };
 
 module.exports = nextConfig;
