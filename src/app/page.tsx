c6cb9a7472ad4829adea3623e9af1c35eec1c150
nida <nida@code.id>
2024-09-17 13:22:47 +0700

    feat: add metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Head>
+        <meta property="og:description" content="Test static export nextjs" />
+        <meta property="og:title" content="Test" />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dlce2r3mg/image/upload/v1679982526/cld-sample-4.jpg"
+        />
+      </Head>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         TEST
       </main>
