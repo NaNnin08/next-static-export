c9449e8f1e47ba7b2c8df7a64f1e31b0ba4975f7
nida <nida@code.id>
2024-09-17 13:27:41 +0700

    feat: change metadata #2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
-import Head from "next/head";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Test",
+  description: "Test static export nextjs",
+  openGraph: {
+    title: "Test",
+    description: "Test static export nextjs",
+    images:
+      "https://res.cloudinary.com/dlce2r3mg/image/upload/v1679982526/cld-sample-3.jpg",
+  },
+};
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Head>
-        <meta property="og:description" content="Test static export nextjs" />
-        <meta property="og:title" content="Test" />
-        <meta
-          property="og:image"
-          content="https://res.cloudinary.com/dlce2r3mg/image/upload/v1679982526/cld-sample-4.jpg"
-        />
-      </Head>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         TEST
       </main>
