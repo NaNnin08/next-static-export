9161fa423ace12656b732142c1597c20adf0921c
nida <nida@code.id>
2024-11-01 10:47:16 +0700

    test

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true, // Optional jika menggunakan Next.js Images, GitHub Pages tidak mendukung Image Optimization.
-  },
-  assetPrefix: process.env.NEXT_PUBLIC_MODE === "dev" ? "" : "./", // Ini untuk memastikan resource diakses dengan path yang benar di GitHub Pages.
-  basePath: process.env.NEXT_PUBLIC_MODE === "dev" ? "" : "/next-static-export", // Ganti dengan nama repositori GitHub Anda
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
 
