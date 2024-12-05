940999370265c4a6eb0bcc63e3ef9a47a51a71f4
nida <nida@code.id>
2024-12-05 16:33:46 +0700

    feat: add header cross-origin

--- next.config.js
+++ next.config.js
@@ -21,14 +21,14 @@ const nextConfig = {
             key: "Cross-Origin-Opener-Policy",
             value: "same-origin",
           },
-          //   {
-          //     key: "Cross-Origin-Embedder-Policy",
-          //     value: "require-corp",
-          //   },
-          //   {
-          //     key: "Cross-Origin-Resource-Policy",
-          //     value: "same-origin",
-          //   },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "unsafe-none",
+          },
+          {
+            key: "Cross-Origin-Resource-Policy",
+            value: "cross-origin",
+          },
         ],
       },
     ];
