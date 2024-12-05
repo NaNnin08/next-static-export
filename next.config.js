f9401d2b2017ec73d3567c7a362e51b694874054
nida <nida@code.id>
2024-12-05 16:26:33 +0700

    feat: add header

--- next.config.js
+++ next.config.js
@@ -6,8 +6,28 @@ const nextConfig = {
         source: "/(.*)",
         headers: [
           {
-            key: "x-frame-options",
+            key: "X-Frame-Options",
             value: "SAMEORIGIN",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Resource-Policy",
+            value: "same-origin",
           },
         ],
       },
